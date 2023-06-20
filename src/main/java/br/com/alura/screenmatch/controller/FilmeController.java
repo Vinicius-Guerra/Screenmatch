1d7cda970e61994fb97bc6fca4cd8521ef6d944e
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-06-20 14:34:25 -0300

    Update FilmeController.java

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -2,6 +2,7 @@ package br.com.alura.screenmatch.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FilmeController {
     @GetMapping
     public String carregaPaginaFormulario() {
+        return "filmes/formulario";
+    }
+    @PostMapping
+    public String cadastraFilme() {
         return "filmes/formulario";
     }
 
